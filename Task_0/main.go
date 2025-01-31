f9c76c3b41ebe17761f03d0e10814737f2b6246b
sejoroajose <53483859+sejoroajose@users.noreply.github.com>
2025-01-31 12:39:07 -0500

    basic api

---
+++ Task_0/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+    "github.com/gorilla/handlers"
+)
+
+type User struct {
+    Email               string  `json:"email"`
+    Current_datetime    string  `json:"current_datetime"`
+    Github_url          string  `json:"github_url"`
+}
+
+
+func getUserInfo(w http.ResponseWriter, r *http.Request){
+    user := User{
+        Email:  "ajosesejoro@gmail.com",
+        Current_datetime: time.Now().Format(time.RFC3339),
+        Github_url: "https://github.com/sejoroajose/HNG-INTERNSHIP", 
+    }
+
+    w.Header().Set("content-Type", "application/json")
+
+    json.NewEncoder(w).Encode(user)
+}
+
+func main() {
+    router := mux.NewRouter()
+
+    router.HandleFunc("/api/user", getUserInfo).Methods("GET")
+
+    corsMiddleware := handlers.CORS(
+        handlers.AllowedOrigins([]string{"*"}),                
+        handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+        handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+    )
+
+    handler := corsMiddleware(router)
+
+    log.Println("Server starting on port 8080...")
+    if err := http.ListenAndServe(":8080", handler); err !=nil{
+        log.Fatal(err)
+    }
+}
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module hng-internship
+
+go 1.23.3
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
